e36a04513962f524561f64f619c0d39388f47790
spread5789 <73854324+spread5789@users.noreply.github.com>
2021-04-10 04:57:54 +0900

    Add files via upload

---
+++ 04.09/체육복/Solution-풀이중2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        int max = n - lost.length;
+
+        for (int i = 0; i < lost.length; i++)
+            for (int j = 0; j < reserve.length; j++)
+                if (lost[i] == reserve[j] || Math.abs(lost[i] - reserve[j]) == 1) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    max++;
+                    break;
+                }
+
+        return max;
+    }
+}
