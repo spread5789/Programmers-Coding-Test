bfa770a6bfd446678f3ebd8e953cd9d6deb8f84e
spread5789 <73854324+spread5789@users.noreply.github.com>
2021-04-20 19:31:54 +0900

    Update Solution.java

--- Coding-Practice/체육복/Solution.java
+++ Coding-Practice/체육복/Solution.java
@@ -5,19 +5,20 @@ class Solution {
         // 최대값을 결정하는 변수 = 최대 학생수 - 잃어버린 학생수 로 
         int max = n - lost.length;
         
-        // 인접한 번호를 가진 학생 체크
+        // 본인이 잃어버렸지만, 여분을 가지고 있었을 경우 먼저 체크
         for (int i = 0; i < lost.length; i++)
             for (int j = 0; j < reserve.length; j++)
                 if (lost[i] == reserve[j]) {
-                    lost[i] = -1;
+                    lost[i] = -1;            // 받았거나, 여분을 빌려줬을 경우 확인을 위해 -1로 체크
                     reserve[j] = -1;
                     max++;  // 여분을 가진 학생이 인접했을경우 추가
                     break;
                 }
         
+        // 잃어버렸고, 다른 친구에게 여분을 빌릴 경우 (그리도 알고리즘 특성 상, 이렇게 위에서 1번, 여기서 1번 체크해줘야한다.)
         for (int i = 0; i < lost.length; i++)
             for (int j = 0; j < reserve.length; j++)
-                if (Math.abs(lost[i] - reserve[j]) == 1) {
+                if (Math.abs(lost[i] - reserve[j]) == 1) { // 절대값으로, 바로 옆번호인 친구일 
                     reserve[j] = -1;
                     max++;
                     break;
