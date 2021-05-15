686f60cef9cc084dfb0eeefed12de83a9069b196
spread5789 <spread5789@gmail.com>
2021-05-15 12:01:45 +0900

    짝수와 홀수

---
+++ Coding-Practice/짝수와 홀수/ReadMe.md
@@ -0,0 +1,4 @@
+문제
+![캡처](https://user-images.githubusercontent.com/73854324/118346226-d8056c80-b574-11eb-8459-3bda4360ddc3.PNG)
+<br><br>
+==
---
+++ Coding-Practice/짝수와 홀수/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int num) {
+        return num%2 == 0 ? "Even" : "Odd" ;
+    }
+}
+
+/*
+class Solution {
+    public String solution(int num) {
+        if(num % 2 == 0)
+            return "Even";
+        return "Odd";
+    }
+}
+*/
