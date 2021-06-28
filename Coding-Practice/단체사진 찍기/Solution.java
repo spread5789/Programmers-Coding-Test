5dc1597450262b8d3f1debbb14831c1ed90796bc
spread5789 <spread5789@gmail.com>
2021-06-28 13:05:43 +0900

    단체사진 찍기

--- Coding-Practice/단체사진 찍기/Solution.java
+++ Coding-Practice/단체사진 찍기/Solution.java
@@ -3,40 +3,38 @@ class Solution {
     char[] position;
     boolean[] visited;
     int answer;
-    
+
     public int solution(int n, String[] data) {
-        
+
         answer = 0;
         position = new char[name.length];
         visited = new boolean[name.length];
-            
+
         dfs(n, data, 0);
         return answer;
     }
-    
-    
-    
+
+
+
     public void dfs(int n, String[] data, int deep){
         if(deep == name.length){
             if(checkCondition(position, data, n))
                 answer++;
             return ;
         }
-        
+
         for(int i=0;i<name.length;i++){
             if(!visited[i]){
                 visited[i] = true;
                 position[deep] = name[i];
-                
-                if(checkCondition(position, data, n))
-                    dfs(n, data, deep+1);
-                
+                dfs(n, data, deep+1);
+
                 visited[i] = false;
             }
-        }   
+        }
     }
-    
-    
+
+
     public boolean checkCondition(char[] position, String[] data, int n){
         for(int i=0;i<n;i++){
             char a = data[i].charAt(0);
@@ -44,12 +42,13 @@ class Solution {
             int a_idx=-1, b_idx=-1;
             char compare = data[i].charAt(3);
             int condition = data[i].charAt(4)-'0';
-            
-            for(int j=0;j<n;j++){
+
+            for(int j=0;j<position.length;j++){
                 if(position[j] == a) a_idx = j;
                 if(position[j] == b) b_idx = j;
+                if(a_idx != -1 && b_idx != -1) break;
             }
-            
+
             int gap = Math.abs(a_idx - b_idx)-1;
             switch(compare){
                 case '=' :
@@ -63,8 +62,8 @@ class Solution {
                     break;
             }
         }
-        
+
         return true;
     }
-    
+
 }
