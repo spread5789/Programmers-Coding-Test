f7e381f67dc79c6fb6ef2b5ec1f19b6f645f3db0
spread5789 <spread5789@gmail.com>
2021-07-04 15:59:45 +0900

    영어 끝말잇기

--- Coding-Practice/영어 끝말잇기/Solution.java
+++ Coding-Practice/영어 끝말잇기/Solution.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int who=1, count=1, order;
-        ArrayList<String> list = new ArrayList<>();
-        
         if(words.length==1) return new int[]{0,0};
         
+        int count=1, who, order;
+        ArrayList<String> list = new ArrayList<>();
         list.add(words[0]);
+        
         for(int i=1;i<words.length;i++){
             count++;
-            order=(n%3==0)?(count/n):(count/n)+1;
             who = (count%n==0)?n:(count%n);
+            order=(count%n==0)?(count/n):(count/n)+1;
             
             if(list.contains(words[i]) || !words[i-1].substring(words[i-1].length()-1).equals(words[i].substring(0,1)))
                 return new int[]{who,order};
