2a5b71016b27b2216052a5a03d3c905fd0785269
spread5789 <spread5789@gmail.com>
2021-07-04 15:54:35 +0900

    Update Solution.java

--- Coding-Practice/영어 끝말잇기/Solution.java
+++ Coding-Practice/영어 끝말잇기/Solution.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int count1=1, count2, who=1;
+        int who=1, count=1, order;
         ArrayList<String> list = new ArrayList<>();
+        
+        if(words.length==1) return new int[]{0,0};
         
         list.add(words[0]);
         for(int i=1;i<words.length;i++){
-            count1++;
-            count2=(count1/n)+1;
-            who++;
-            who = (who%n==0)?1:who;
+            count++;
+            order=(n%3==0)?(count/n):(count/n)+1;
+            who = (count%n==0)?n:(count%n);
             
             if(list.contains(words[i]) || !words[i-1].substring(words[i-1].length()-1).equals(words[i].substring(0,1)))
-                return new int[]{who,count2};
+                return new int[]{who,order};
             
             list.add(words[i]);
         }
