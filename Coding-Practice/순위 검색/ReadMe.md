e18ed5d0d312929184a6b04a7f378ee32deacd64
spread5789 <spread5789@gmail.com>
2021-07-20 12:07:10 +0900

    순위 검색

---
+++ Coding-Practice/순위 검색/ReadMe.md
@@ -0,0 +1,9 @@
+문제
+==
+![1](https://user-images.githubusercontent.com/73854324/126255857-e1851d76-778c-47f0-b4b0-6075a5a31c9a.PNG)
+![2](https://user-images.githubusercontent.com/73854324/126255862-0d88754d-3617-4356-b2f0-2efc311892cc.PNG)
+![3](https://user-images.githubusercontent.com/73854324/126255867-1b4ad5dc-e37e-4f5f-bb07-aa507a0a9ba3.PNG)
+![4](https://user-images.githubusercontent.com/73854324/126255871-60c4a3f5-ca4b-4a31-8d88-4cf21539e0c3.PNG)
+<br><br>
+==
+테스트는 맞았는데 제출에서 다 틀렸다.. 피드백을 받고싶은데 받아줄 사람이 없다.
---
+++ Coding-Practice/순위 검색/Solution(Fail).java
@@ -0,0 +1,42 @@
+class Solution {
+    int sum;
+
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = new int[info.length];
+        
+        for(int z=0;z<info.length;z++){
+            // 지원자 정보 (String[][] cand)
+            String[][] cand = new String[info.length][5];
+            for(int i=0;i<info.length;i++){
+                cand[i] = info[i].split(" ");
+            }
+
+            // 조건 정보 (String[] cond)
+            String[] cond = query[z].replaceAll("and ","").split(" ");
+
+            sum = 0;
+            // 각 조건 별 지원자 체크
+            for(int i=0;i<cond.length;i++){
+                for(int j=0;j<cand[i].length;j++){
+                    if(j==4){
+                        int a = Integer.parseInt(cand[i][4]);
+                        int b = Integer.parseInt(cond[4]);
+                        if(a >= b){
+                            sum++;
+                            break;
+                        }
+                    }
+
+                    if(!cond[j].equals(cand[i][j]) && !cond[j].equals("-")){
+                        break;
+                    }
+                }
+            }
+
+            answer[z] = sum;
+
+        }
+
+        return answer;
+    }
+}
